757b5bf42f19da9c600f327b51a81beae6fce10c
JairGuzman1810 <jair18101@hotmail.com>
2024-10-26 21:20:13 -0600

    Fixed Object Placement and Added Game Executable
    - Resolved issue with misplaced objects in new map setup.
    - Added .jar file for game distribution and execution.
    - Completed functionality and final setup for Treasure Hunt game.

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -21,10 +21,10 @@ public class AssetSetter {
     // The setObject method places various game objects (keys, doors, and a chest)
     // at specific world coordinates using the GamePanel's tile size.
     public void setObject() {
-        // Set a key at coordinates (23, 7) in the world.
+        // Set a key at coordinates (23, 12) in the world.
         gp.obj[0] = new OBJ_Key(gp);
         gp.obj[0].worldX = 23 * gp.tileSize;
-        gp.obj[0].worldY = 7 * gp.tileSize;
+        gp.obj[0].worldY = 12 * gp.tileSize;
 
         // Set another key at coordinates (23, 40) in the world.
         gp.obj[1] = new OBJ_Key(gp);
@@ -36,27 +36,27 @@ public class AssetSetter {
         gp.obj[2].worldX = 38 * gp.tileSize;
         gp.obj[2].worldY = 8 * gp.tileSize;
 
-        // Set a door at coordinates (10, 11) in the world.
+        // Set a door at coordinates (10, 12) in the world.
         gp.obj[3] = new OBJ_Door(gp);
         gp.obj[3].worldX = 10 * gp.tileSize;
-        gp.obj[3].worldY = 11 * gp.tileSize;
+        gp.obj[3].worldY = 12 * gp.tileSize;
 
         // Set another door at coordinates (8, 28) in the world.
         gp.obj[4] = new OBJ_Door(gp);
         gp.obj[4].worldX = 8 * gp.tileSize;
         gp.obj[4].worldY = 28 * gp.tileSize;
 
-        // Set another door at coordinates (12, 22) in the world.
+        // Set another door at coordinates (12, 23) in the world.
         gp.obj[5] = new OBJ_Door(gp);
         gp.obj[5].worldX = 12 * gp.tileSize;
-        gp.obj[5].worldY = 22 * gp.tileSize;
+        gp.obj[5].worldY = 23 * gp.tileSize;
 
-        // Set a chest at coordinates (10, 7) in the world.
+        // Set a chest at coordinates (10, 8) in the world.
         gp.obj[6] = new OBJ_Chest(gp);
         gp.obj[6].worldX = 10 * gp.tileSize;
-        gp.obj[6].worldY = 7 * gp.tileSize;
+        gp.obj[6].worldY = 8 * gp.tileSize;
 
-        // Set boots at coordinates (10, 7) in the world.
+        // Set boots at coordinates (37, 42) in the world.
         gp.obj[7] = new OBJ_Boots(gp);
         gp.obj[7].worldX = 37 * gp.tileSize;
         gp.obj[7].worldY = 42 * gp.tileSize;
